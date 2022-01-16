e732768cee01119a5008e6a0d99d9bd172bb7bc2
Muhammad Noamany <muhammadnoamany@gmail.com>
2022-01-16 19:49:00 +0200

    ## add video player and resume on orientation change capability

--- app/src/main/java/com/alphaapps/androidpickerapp/ui/picked_details/PickedDetailsActivity.java
+++ app/src/main/java/com/alphaapps/androidpickerapp/ui/picked_details/PickedDetailsActivity.java
@@ -3,7 +3,9 @@ package com.alphaapps.androidpickerapp.ui.picked_details;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,6 +16,11 @@ import com.alphaapps.androidpickerapp.ui.base.BaseActivity;
 import com.alphaapps.pickermodule.data.ResultData;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.io.File;
 
@@ -23,6 +30,9 @@ import java.io.File;
  */
 public class PickedDetailsActivity extends BaseActivity<ActivityPickedDetailsBinding> {
     private DetailsVM detailsVM;
+    private SimpleExoPlayer player;
+    private DefaultDataSourceFactory dataSourceFactory;
+    private ResultData fileData;
 
     /**
      * start activity helper method
@@ -50,6 +60,17 @@ public class PickedDetailsActivity extends BaseActivity<ActivityPickedDetailsBin
         checkExtras();
     }
 
+    /**
+     * get the intent extras
+     */
+    private void checkExtras() {
+        Bitmap resultBitmap = getIntent().getExtras().getParcelable("resultBitmap");
+        ResultData resultData = getIntent().getExtras().getParcelable("resultData");
+        detailsVM.setPickedFileData(resultData);
+        detailsVM.setTakenCameraPhoto(resultBitmap);
+
+    }
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_picked_details;
@@ -69,6 +90,7 @@ public class PickedDetailsActivity extends BaseActivity<ActivityPickedDetailsBin
     private void observeData() {
         detailsVM.getPickedFileData().observe(this, resultData -> {
             if (resultData != null) {
+                fileData = resultData;
                 setResultData(resultData);
             }
         });
@@ -84,6 +106,7 @@ public class PickedDetailsActivity extends BaseActivity<ActivityPickedDetailsBin
      * Set observe data result
      */
     private void setResultData(Bitmap bitmap) {
+        binding.imageContainer.setVisibility(View.VISIBLE);
         binding.imageContainer.setImageBitmap(bitmap);
         binding.widthTV.setText(bitmap.getWidth() + "");
         binding.heightTV.setText(bitmap.getHeight() + "");
@@ -93,19 +116,58 @@ public class PickedDetailsActivity extends BaseActivity<ActivityPickedDetailsBin
      * Set observe data result
      */
     private void setResultData(ResultData resultData) {
-        if (!resultData.isVideo())
+        if (!resultData.isVideo()) {
             Glide.with(PickedDetailsActivity.this).load(new File(resultData.getFilePath())).apply(new RequestOptions().sizeMultiplier(0.5f)).into(binding.imageContainer);
+            binding.imageContainer.setVisibility(View.VISIBLE);
+        } else {
+            binding.playerView.setVisibility(View.VISIBLE);
+            initPlayerView();
+            preparePlayer();
+        }
         binding.setDataModel(resultData);
     }
 
     /**
-     * get the intent extras
+     * Init of the ExoPlayer
      */
-    private void checkExtras() {
-        Bitmap resultBitmap = getIntent().getExtras().getParcelable("resultBitmap");
-        ResultData resultData = getIntent().getExtras().getParcelable("resultData");
-        detailsVM.setPickedFileData(resultData);
-        detailsVM.setTakenCameraPhoto(resultBitmap);
+    private void initPlayerView() {
+        player = new SimpleExoPlayer.Builder(PickedDetailsActivity.this).build();
+        binding.playerView.setPlayer(player);
+        dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, getString(R.string.app_name)));
+    }
+
+    /**
+     * Prepare ExoPlayer
+     */
+    private void preparePlayer() {
+        MediaSource videoSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(fileData.getFilePath()));
+        player.prepare(videoSource);
+        if (detailsVM.getPositionMilliSecondData().getValue() > 0)
+            resumeExoPlayer();
+        player.setPlayWhenReady(true);
+    }
+
+    /**
+     * Resume ExoPlayer
+     */
+    private void resumeExoPlayer() {
+        player.seekTo(detailsVM.getWindowIndexData().getValue(), detailsVM.getPositionMilliSecondData().getValue());
+    }
+
+    /**
+     * release Exo Player on Activity pause
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null && player.isPlaying()) {
+            detailsVM.setWindowIndex(player.getCurrentWindowIndex());
+            detailsVM.setPositionMilliSecond(Math.max(0, player.getContentPosition()));
+            player.release();
+        }
 
     }
+
 }
